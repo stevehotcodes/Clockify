52f75d6071479fda3ef11808f82d0100303d63d1
Stephen Ondieki <stevehotcodes@gmail.com>
2024-03-09 23:19:40 +0300

    (feat) added the validation for the login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import '../Login/Login.scss'
 import LoginFormbackgroundImage from '../../assets/login_background_image.avif'
 import logo from '../../assets/Clockify-logo.png'
+import {useForm} from 'react-hook-form'
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
+
+
+
 
 const Login = () => {
+
+    const schema=yup.object().shape({
+        email:yup.string().email().required("email is required"),
+        password:yup.string().min(4).max(20).required("password is required "),
+
+    })
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver:yupResolver(schema)
+      });
+     
+      const onSubmit=(data)=>{
+        console.log(data)
+    }
+
+
+
+
+
+
   return (
     <div className='login-layout'>
         <div className='login-container'>
@@ -12,18 +42,34 @@ const Login = () => {
                 <img src={logo} alt=""  className='logo'/>
             </div>
             <div className='login-form-wrapper'>
-                <form  className='login-form'>
+                <form  className='login-form'  onSubmit={handleSubmit(onSubmit)}>
                     
                 <h3>Hi Welcome back </h3>
                 <span>Please fill in your details to log in</span>
                 <div class="label-input-group">
                     <label>Email</label><br/>
-                    <input type="text" name="" id="" placeholder="Email" />                  
+                    <input 
+                        type="text"
+                         name="" id="" 
+                         placeholder="Email" 
+                         {...register("email")}
+                        
+                         />  
+                         <p className='error-message'>{errors.email?.message}</p>   
+
                 </div>
     
                 <div class="label-input-group">
                     <label>Password</label><br/>
-                    <input type="password" name="" id="" placeholder="Password" />
+                    <input 
+                        type="password"
+                         name="" id="" 
+                         placeholder="Password"
+                         {...register("password")}
+                         
+                         />
+
+                         <p className='error-message'>{errors.password?.message}</p>
                  
                 </div>
     
