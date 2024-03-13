8b8c9036a09d12aea7e8ab5377976629f2f817e6
Stephen Ondieki <stevehotcodes@gmail.com>
2024-03-13 23:11:25 +0300

    done the middleware

---
+++ api/src/middlewares/useAuthMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { notAuthorized } from "../helpers/helper.function.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const verifyUserIdentity = (req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRET, (err, decode) =>  {
+            if (err) {
+                
+                return notAuthorized(res, 'Please login or create an account ');
+            } else {
+                req.user = decode;
+                console.log(req.user)
+                next();
+            }
+        });
+    } else {
+        return notAuthorized(res, 'Access denied');
+    }
+
+} 
